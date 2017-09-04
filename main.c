fa45f40970a0d86a5c7296a102ad6fca427f4c8e
Kimanh1905 <anhntkd00600@fpt.edu>
2017-09-05 00:35:14 +0700

    bài tập ngày 31-08-2017

--- main.c
+++ main.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
 main() {
-	printf("%i \n",multiply(12,2));
-	printf("%i", devision(12, 2));
-	}
-	//nhan hai so voi tham so dau vao 
+	int a;
+	int b;
+	printf("so cua a la:");
+	scanf("%i", &a);
+	printf("so cua b la:");
+	scanf("%i", &b);
+	printf("%i\n", plus(a,b));
+	printf("%i\n", minus(a,b));
+	printf ("%i\n", multiply (a,b));
+	printf ("%i", devision (a,b));
+
+
+}
+	//tinh tong, hieu, nhân, chia cua hai so bat ki 
 	// la a, b la hai so nguyen.
+
+	int plus (int a,int b){
+		return a + b ;
+}
+	int minus (int a,int b){
+		return a - b;
+}
 	int multiply (int a,int b){
 		return a * b;
 }
 	int devision ( int a, int b){
 		return a / b;
-		}
-	
+}		
+	 
+	 
